fc1e2c77e77560e490781ff0943f85237b48596a
Emrullah10 <kemalbozkurt1006@gmail.com>
2023-03-30 01:31:11 +0300

    Add project files.

---
+++ sinema_büfe_satis/Form1.Designer.cs
@@ -0,0 +1,353 @@
+﻿
+namespace sinema_büfe_satis
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtbilet = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtcay = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtsu = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtmisir = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblkasa = new System.Windows.Forms.Label();
+            this.lbltoplam = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.groupBox1.Controls.Add(this.txtbilet);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.txtcay);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtsu);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtmisir);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 124);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(171, 227);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Ürün Miktar";
+            // 
+            // txtbilet
+            // 
+            this.txtbilet.Location = new System.Drawing.Point(49, 168);
+            this.txtbilet.Name = "txtbilet";
+            this.txtbilet.Size = new System.Drawing.Size(100, 23);
+            this.txtbilet.TabIndex = 1;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe MDL2 Assets", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(6, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 21);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Bilet:";
+            // 
+            // txtcay
+            // 
+            this.txtcay.Location = new System.Drawing.Point(49, 125);
+            this.txtcay.Name = "txtcay";
+            this.txtcay.Size = new System.Drawing.Size(100, 23);
+            this.txtcay.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe MDL2 Assets", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(6, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 21);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Çay:";
+            // 
+            // txtsu
+            // 
+            this.txtsu.Location = new System.Drawing.Point(49, 80);
+            this.txtsu.Name = "txtsu";
+            this.txtsu.Size = new System.Drawing.Size(100, 23);
+            this.txtsu.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe MDL2 Assets", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(6, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 21);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Su:";
+            // 
+            // txtmisir
+            // 
+            this.txtmisir.Location = new System.Drawing.Point(49, 38);
+            this.txtmisir.Name = "txtmisir";
+            this.txtmisir.Size = new System.Drawing.Size(100, 23);
+            this.txtmisir.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe MDL2 Assets", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(6, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mısır:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label6);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Location = new System.Drawing.Point(222, 124);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(171, 227);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Ürün Fiyat";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Segoe MDL2 Assets", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label8.Location = new System.Drawing.Point(16, 166);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 21);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Bilet: 8TL";
+            this.label8.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe MDL2 Assets", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(16, 123);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(71, 21);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Çay: 2TL";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe MDL2 Assets", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(16, 78);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(63, 21);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Su: 1TL";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe MDL2 Assets", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(16, 40);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 21);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Mısır: 4 TL";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.lblkasa);
+            this.groupBox3.Controls.Add(this.lbltoplam);
+            this.groupBox3.Controls.Add(this.label12);
+            this.groupBox3.Controls.Add(this.label9);
+            this.groupBox3.Location = new System.Drawing.Point(437, 124);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(171, 228);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Kasa";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(16, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(131, 35);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "TEMİZLE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(16, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 35);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblkasa
+            // 
+            this.lblkasa.AutoSize = true;
+            this.lblkasa.Font = new System.Drawing.Font("Segoe MDL2 Assets", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblkasa.Location = new System.Drawing.Point(76, 187);
+            this.lblkasa.Name = "lblkasa";
+            this.lblkasa.Size = new System.Drawing.Size(49, 21);
+            this.lblkasa.TabIndex = 0;
+            this.lblkasa.Text = "00 TL";
+            // 
+            // lbltoplam
+            // 
+            this.lbltoplam.AutoSize = true;
+            this.lbltoplam.Font = new System.Drawing.Font("Segoe MDL2 Assets", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lbltoplam.Location = new System.Drawing.Point(76, 52);
+            this.lbltoplam.Name = "lbltoplam";
+            this.lbltoplam.Size = new System.Drawing.Size(49, 21);
+            this.lbltoplam.TabIndex = 0;
+            this.lbltoplam.Text = "00 TL";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Segoe MDL2 Assets", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label12.Location = new System.Drawing.Point(32, 187);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(48, 21);
+            this.label12.TabIndex = 0;
+            this.label12.Text = "Kasa:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Segoe MDL2 Assets", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(16, 52);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(64, 21);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "Toplam:";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Pristina", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(18, 23);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(298, 39);
+            this.label11.TabIndex = 2;
+            this.label11.Text = "Sinema & Büfe Satış Panali";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::sinema_büfe_satis.Properties.Resources.ticket;
+            this.pictureBox1.Location = new System.Drawing.Point(437, -6);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(147, 124);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 365);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Segoe MDL2 Assets", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtmisir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtbilet;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtcay;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtsu;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lbltoplam;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblkasa;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ sinema_büfe_satis/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sinema_büfe_satis
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+        int kasatutar = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int misir, bilet, su, cay,toplam;
+            misir = Convert.ToInt16(txtmisir.Text);
+            bilet = Convert.ToInt16(txtbilet.Text);
+            su = Convert.ToInt16(txtsu.Text);
+            cay = Convert.ToInt16(txtcay.Text);
+
+
+            toplam = misir * 4 + bilet * 8 + su * 1 + cay * 2;
+            lbltoplam.Text = toplam.ToString() + " TL";
+
+            kasatutar = kasatutar + toplam;
+            lblkasa.Text = kasatutar.ToString() + " TL";
+             
+        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtbilet.Text = "";
+            txtcay.Text = "";
+            txtmisir.Text = "";
+            txtsu.Text = "";
+            txtmisir.Focus();
+        }
+    }
+}
